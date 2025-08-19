37690f58ca4d17b557d6d0cf084ad8e3ba363365
danahparis21 <micadanah21@gmail.com>
2025-08-19 22:45:38 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="text-3xl font-bold text-pink-500">
+      🍱 Meal Planner works!
+    </div>
+  )
+}
+
+export default App
